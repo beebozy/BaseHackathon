fb2ea15f6c7faaeb95eaf0c70837c3128fc41f2b
beebozy <musahabeeblai@gmail.com.com>
2024-10-12 21:54:15 +0100

    contract deployed and verified

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 
 ## Table of Contents
 
+# contract Adress 0xD3Bf935b00647732888D785C2674965fd803F8b9
+
+# Verification of contract  https://sepolia.basescan.org/address/0xD3Bf935b00647732888D785C2674965fd803F8b9#code
 - [Description](#description)
 - [Features](#features)
 - [Tech Stack](#tech-stack)
@@ -19,11 +22,11 @@ We are building a decentralized invoicing system with the goal of solving trust 
 
 - Key features include:
    - Create and store invoices on the blockchain
-   - Payment of invoices via both ERC20 tokens (LISK) and Ether (ETH)
+   - Payment of invoices via both ERC20 tokens (BASE) and Ether (ETH)
    - Recording customer details and associating them with invoice transactions
    - Secure fund transfers from buyer to seller once an invoice is paid
    - Invoice creation: Sellers can create invoices for buyers, detailing goods, the total amount, and other information
-   - Payment in cryptocurrency: Buyers can pay invoices in LISK
+   - Payment in cryptocurrency: Buyers can pay invoices in BASE
    - Payment in Ether: Buyers also have the option to pay in ETH directly
    - Customer registration: The system allows customer registration to link customer details with invoice transactions
 
--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,34 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
+import { vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
-dotenv.config();
+
+
+
+
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY")
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     // for testnet
-    "lisk-sepolia": {
-      url: process.env.LISK_RPC_URL!,
-      accounts: [
-        process.env.ACCOUNT_PRIVATE_KEY!
-      ],
-      gasPrice: 100000000000,
-      
+    //url: 'https://rpc.sepolia-api.lisk.com
+    "base-sepolia": {
+      url: "https://sepolia.base.org",
+      accounts: [SEPOLIA_PRIVATE_KEY],
+      gasPrice: 1000000000,
     },
-    
   },
   etherscan: {
     // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
     apiKey: {
-      "lisk-sepolia": "123",
+      "base-sepolia": "UGXE2P7JTA2NIEIRB23EK7UIBPESQRTVCN",
     },
     customChains: [
       {
-        network: "lisk-sepolia",
-        chainId: 4202,
+        network: "base-sepolia",
+        chainId: 84532,
         urls: {
-          apiURL: "https://sepolia-blockscout.lisk.com/api",
-          browserURL: "https://sepolia-blockscout.lisk.com/",
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org",
         },
       },
     ],
@@ -37,5 +39,8 @@ const config: HardhatUserConfig = {
     enabled: false,
   },
 };
+
+
+
 
 export default config;
